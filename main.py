a4dd655f78ba612c4e874ae9be337cd32ee5337b
Mister-Bajaan <pvpaaseydoubadiane@gmail.com>
2025-09-01 17:39:14 +0000

    feat: Configuration de l'interface utilisateur et test de l'affichage - fenetre, titre

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import Tk
+
+#--------------------- Configuration de l'interface ---------------------#
+
+# Création de la fenêtre
+fenetre = Tk()
+
+# Titre de la fenêtre
+fenetre.title("Rectangle avec 4 points") 
+
+
+# Lancer la fenêtre
+fenetre.mainloop()
